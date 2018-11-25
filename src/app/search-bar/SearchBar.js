0d7e9c66f9d22567134960da1f0f2034cb47ad9d
Robert Fischer <artiefischer@gmail.com>
2018-11-25 11:48:33 -0700

    working on filter search

--- src/app/search-bar/SearchBar.js
+++ src/app/search-bar/SearchBar.js
@@ -1,13 +1,6 @@
 import React from 'react'
 
 export default class SearchBar extends React.Component {
-  constructor(props) {
-    super(props)
-    var updatedList = this.props
-    
-    console.log(updatedList)
-  }
-  
   render() {
     let searchResult
     const filterFunc = (ev) => {
@@ -71,43 +64,3 @@ export default class SearchBar extends React.Component {
     )
   }
 }
-
-
-
-// var FilteredList = React.createClass({
-//   filterList: function (event) {
-//     var updatedList = this.state.books;
-//     updatedList = updatedList.filter(function (item) {
-//       return item.toLowerCase().search(
-//         event.target.value.toLowerCase()) !== -1;
-//     });
-//     this.setState({ books: updatedList });
-//   },
-
-//   render: function () {
-//     return (
-//       <div className="filter-list">
-//         <form>
-//           <fieldset className="form-group">
-//             <input type="text" className="form-control form-control-lg" placeholder="Search" onChange={this.filterList} />
-//           </fieldset>
-//         </form>
-//         <List items={this.state.items} />
-//       </div>
-//     );
-//   }
-// });
-
-// var List = React.createClass({
-//   render: function () {
-//     return (
-//       <ul className="list-group">
-//         {
-//           this.props.items.map(function (item) {
-//             return <li className="list-group-item" data-category={item} key={item}>{item}</li>
-//           })
-//         }
-//       </ul>
-//     )
-//   }
-// });
